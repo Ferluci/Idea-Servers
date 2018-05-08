1e9b24a01893e79372b4d368e5cc5b3fc5853513
Konstantin <anishenko00@gmail.com>
2018-05-09 00:34:20 +0300

    refactor

--- servers.py
+++ servers.py
@@ -2,16 +2,22 @@ import re
 import demjson
 import requests
 
-base_url = 'http://xidea.online/'
-response = requests.get(base_url + 'servers.html')
-servers_file = re.findall(r'assets/js/main-\w*', response.text)[0] + '.js'
-servers_file =requests.get(base_url + servers_file).text
+def get_servers():
+    base_url = 'http://xidea.online/'
+    response = requests.get(base_url + 'servers.html')
+    servers_file = re.findall(r'assets/js/main-\w*', response.text)[0] + '.js'
+    servers_file =requests.get(base_url + servers_file).text
 
-servers_array_start_index = servers_file.index('servers:')
-start_index = servers_array_start_index + len('servers:')
-end_index = servers_file[servers_array_start_index:].index(']') + servers_array_start_index + 1
-servers_file = servers_file[start_index: end_index]
+    servers_array_start_index = servers_file.index('servers:')
+    start_index = servers_array_start_index + len('servers:')
+    end_index = servers_file[servers_array_start_index:].index(']') + servers_array_start_index + 1
+    servers_file = servers_file[start_index: end_index]
 
-servers = demjson.decode(servers_file)
-for server in servers:
-    print(server['url'])
+    return demjson.decode(servers_file)
+
+def print_servers():
+    for server in get_servers():
+        print(server['url'])
+
+if __name__ == '__main__':
+    print_servers()
